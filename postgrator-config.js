8d57e870c3fd42c043e719d8571c87cee83abfb0
Ben Hernandez <benjammin234@gmail.com>
2020-04-07 11:40:52 -0700

    Sorting out deploying

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
 
+console.log(process.env.ssl);
+console.log(process.env.DATABASE_URL);
+
 module.exports = {
   migrationsDirectory: "migrations",
   driver: "pg",
   connectionString:
     process.env.NODE_ENV === "test"
-      ? process.env.TEST_DATABASE_URL
+      ? process.env.TEST_DATABASE_URL + "?sslmode=require"
       : process.env.DATABASE_URL,
   ssl: !!process.env.SSL,
 };
