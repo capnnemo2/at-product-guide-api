f50da89caf0da00f33369ce560aa3a6b5352faeb
Ben Hernandez <benjammin234@gmail.com>
2020-04-16 17:33:45 -0700

    Battling cors

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  CLIENT_ORIGIN: "https://at-product-guide.now.sh/",
+  CLIENT_ORIGIN: "https://at-product-guide.now.sh",
   DATABASE_URL:
     process.env.DATABASE_URL ||
     "postgresql://postgres@localhost/at-product-guide",
